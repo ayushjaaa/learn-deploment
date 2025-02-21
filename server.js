a496f637bf65f4f837f785153ffc396e41579b6c
Aysh Jaiswal <ayushjaiswal452@gmail.com>
2025-02-21 11:44:31 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const app = require('./src/app')
+
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+io.on('connection', socket => {
+ console.log('user connected')
+});
+server.listen(3000,()=>{
+    console.log('sevrer is running on  the port 3000')
+});
+
+
+
